b0f3c63aca44e67a03df21ec4cc53828767c945f
syd-alm <121456136+syd-alm@users.noreply.github.com>
2024-12-03 13:15:52 -0700

    Update pherr-raw_fastqcARRAY.sh

--- pherr-raw_fastqcARRAY.sh
+++ pherr-raw_fastqcARRAY.sh
@@ -3,15 +3,15 @@
 #SBATCH --partition=t1standard
 #SBATCH --nodes=3
 #SBATCH --ntasks-per-node=24
-#SBATCH --job-name=fqc_array_cd
-#SBATCH --output=/center1/GLASSLAB/salmgren/lcwgs/job_outfiles/pherr-raw_fastqc_CD_%A-%a.out
-#SBATCH --error=/center1/GLASSLAB/salmgren/lcwgs/job_outfiles/pherr-raw_fastqc_CD_%A-%a.err
-#SBATCH --array=1-40%5
+#SBATCH --job-name=fastqc_raw
+#SBATCH --output=/center1/GLASSLAB/salmgren/lcwgs/job_outfiles/pherr-raw_fastqc_%A-%a.out
+#SBATCH --error=/center1/GLASSLAB/salmgren/lcwgs/job_outfiles/pherr-raw_fastqc_%A-%a.err
+#SBATCH --array=1-120%5
 
 module purge
 module load Java/11.0.16
 
-JOBS_FILE=/center1/GLASSLAB/salmgren/lcwgs/scripts/pherr-raw_fqcARRAY_input_CD.txt
+JOBS_FILE=/center1/GLASSLAB/salmgren/lcwgs/scripts/pherr-raw_fastqcARRAY_input.txt
 IDS=$(cat ${JOBS_FILE})
 
 for sample_line in ${IDS}
